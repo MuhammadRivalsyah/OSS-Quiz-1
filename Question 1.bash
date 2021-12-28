9eed0fe5e99c3facc25e62dd3af163dc33837fb1
MuhammadRivalsyah <96765784+MuhammadRivalsyah@users.noreply.github.com>
2021-12-28 11:11:23 +0300

    Add files via upload

---
+++ Question 1.bash
@@ -0,0 +1,21 @@
+num=9
+
+w=${1-3}
+
+if [ $((num%2)) -ne 0 ]
+
+then
+ line() { 
+   printf "%$(($1+w))s\n" "$(yes "* " | sed ${1}q | tr -d \n)"; 
+
+   }
+ for i in $(seq $w) $(seq $((w-1)) -1 1); 
+ 
+ do line $i; 
+ 
+ done
+
+ else
+
+   echo "it is not odd"
+fi
