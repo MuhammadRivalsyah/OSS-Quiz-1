177edb5fafc83c442248b5c7aa8b5a13691ab4ff
MuhammadRivalsyah <96765784+MuhammadRivalsyah@users.noreply.github.com>
2021-12-28 11:31:26 +0300

    Update Question 2.bash

--- Question 2.bash
+++ Question 2.bash
@@ -1,20 +1,13 @@
 num=4
-
 w=${1-3}
-
 if [ $((num%2)) -ne 0 ]
-
 then
  line() { 
    printf "%$(($1+w))s\n" "$(yes "* " | sed ${1}q | tr -d \n)"; 
-
    }
  for i in $(seq $w) $(seq $((w-1)) -1 1); 
- 
- do line $i; 
- 
+ do line $i;  
  done
-
  else if [ $rem -eq 0 ]
 then
   echo "$n is even number"
